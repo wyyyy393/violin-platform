372040d50d1a5bbb667ae9e38e7ecf7cf94d08ba
wyyyy393 <oops240526@gmail.com>
2026-06-01 16:38:20 +0800

    update vite config for better browser compatibility

--- vite.config.ts
+++ vite.config.ts
@@ -8,4 +8,11 @@ export default defineConfig({
       '@': '/src',
     },
   },
+  build: {
+    target: ['es2020', 'edge88', 'firefox78', 'chrome87', 'safari14'],
+    cssTarget: ['chrome87'],
+  },
+  server: {
+    host: true,
+  },
 })
